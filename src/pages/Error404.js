321a35c9934a93c7314072d4a6e75e3bcb5b23c8
diegogil <dgg26496@hotmail.com>
2023-07-03 15:55:25 -0600

    Secrean los primeros archivos, hasta el archivo de ruta

---
+++ src/pages/Error404.js
@@ -0,0 +1,8 @@
+const Eror404 = () => {
+    const view = `<div class="Error404">
+    <h2>Error 404</h2>
+</div>`;
+    return view;
+}
+
+export default Eror404;
---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `<div class="Header-main">
+    <div class="Header-log">
+        <h1>
+            <a href="#">
+                Rick and Morty
+            </a>
+        </h1>
+    </div>
+    <div class="Header-nav">
+        <a href="#">
+            about
+        </a>
+    </div>
+</div>`;
+    return view;
+}
+
+export default Header;
